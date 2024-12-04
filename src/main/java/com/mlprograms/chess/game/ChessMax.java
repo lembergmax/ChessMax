d464f1ca5a089c332a8de0c1a24368d81e2492ba
maxle <ml.programs.d@gmail.com>
2024-12-04 15:39:28 +0100

    added copyright

---
+++ src/main/java/com/mlprograms/chess/game/ChessMax.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2024 Max Lemberg. This file is part of ChessMax.
+ * Licenced under the CC BY-NC 4.0 License.
+ * See "http://creativecommons.org/licenses/by-nc/4.0/".
+ */
+
+package com.mlprograms.chess.game;
+
+public class ChessMax {
+
+	public ChessMax() {
+		// TODO: create the game
+	}
+
+	public void play() {
+		// TODO: start the game
+	}
+
+}
