dff33a3eb448bd65ff6e66740d2c562c972f8496
maxle <ml.programs.d@gmail.com>
2024-12-06 12:18:15 +0100

    created account Manager class

---
+++ src/main/java/com/mlprograms/chess/account/AccountManager.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2024 Max Lemberg. This file is part of ChessMax.
+ * Licenced under the CC BY-NC 4.0 License.
+ * See "http://creativecommons.org/licenses/by-nc/4.0/".
+ */
+
+package com.mlprograms.chess.account;
+
+public class AccountManager {
+}
