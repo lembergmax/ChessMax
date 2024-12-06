849ef93ddbea345eb20a49c276b18a136ab5eb9c
maxle <ml.programs.d@gmail.com>
2024-12-06 12:00:06 +0100

    added few comments and renamed loadUI to initializeUI

--- src/main/java/com/mlprograms/chess/Main.java
+++ src/main/java/com/mlprograms/chess/Main.java
@@ -6,15 +6,29 @@
 
 package com.mlprograms.chess;
 
-import com.mlprograms.chess.game.ChessMax;
+import com.mlprograms.chess.ui.Startmenu;
 
+/**
+ * The main entry point for the ChessMax application.
+ * Initializes and displays the user interface.
+ */
 public class Main {
 
-	public static void main(String[] args) {
+    /**
+     * Main method to start the application.
+     *
+     * @param args command-line arguments (not used)
+     */
+    public static void main(String[] args) {
+        initializeUI();
+    }
 
-		ChessMax chessMax = new ChessMax();
-		chessMax.start();
-
-	}
+    /**
+     * Loads and displays the start menu of the application.
+     */
+    private static void initializeUI() {
+        Startmenu startmenu = new Startmenu(); // Create a new instance of the Startmenu UI
+        startmenu.setVisible(true); // Make the start menu visible
+    }
 
 }
