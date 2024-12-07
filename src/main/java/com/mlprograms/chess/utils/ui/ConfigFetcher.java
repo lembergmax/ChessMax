7994408d6120da7a573890e12a4809c1867be0f0
maxle <ml.programs.d@gmail.com>
2024-12-07 13:54:18 +0100

    return Color instead of string

--- src/main/java/com/mlprograms/chess/utils/ui/ConfigFetcher.java
+++ src/main/java/com/mlprograms/chess/utils/ui/ConfigFetcher.java
@@ -8,39 +8,60 @@ package com.mlprograms.chess.utils.ui;
 
 import com.mlprograms.chess.utils.ConfigReader;
 
+import java.awt.*;
+
 public class ConfigFetcher {
 
 	private static final ConfigReader CONFIG_READER = new ConfigReader();
 
 	/**
-	 * Fetches a string configuration value from the ChessGame section.
+	 * Fetches a string configuration value based on the given section and key.
 	 *
-	 * @param key
-	 * 	The key of the configuration value.
 	 * @param section
-	 * 	The section of the configuration file.
+	 * 	the configuration section to look into (e.g., "General", "Settings").
+	 * @param key
+	 * 	the key whose value is to be fetched from the specified section.
 	 *
-	 * @return The string value associated with the key.
+	 * @return the configuration value as a String.
 	 */
 	public static String fetchStringConfig(String section, String key) {
 		return CONFIG_READER.getValue(section, key);
 	}
 
 	/**
-	 * Fetches an integer configuration value from the ChessGame section.
+	 * Fetches an integer configuration value based on the given section and key.
+	 * <p>
+	 * This method retrieves the value as a string and converts it to an integer.
 	 *
-	 * @param key
-	 * 	The key of the configuration value.
 	 * @param section
-	 * 	The section of the configuration file.
+	 * 	the configuration section to look into (e.g., "General", "Settings").
+	 * @param key
+	 * 	the key whose value is to be fetched from the specified section.
 	 *
-	 * @return The integer value associated with the key.
+	 * @return the configuration value as an integer.
 	 *
 	 * @throws NumberFormatException
-	 * 	if the configuration value is not a valid integer.
+	 * 	if the fetched value is not a valid integer.
 	 */
 	public static int fetchIntegerConfig(String section, String key) {
 		return Integer.parseInt(fetchStringConfig(section, key));
+	}
+
+	/**
+	 * Fetches an integer configuration value from the "Colors" section based on the provided key.
+	 * <p>
+	 * This method is specialized for retrieving color-related configuration values.
+	 *
+	 * @param key
+	 * 	the key whose value is to be fetched from the "Colors" section.
+	 *
+	 * @return the configuration value as an integer.
+	 *
+	 * @throws NumberFormatException
+	 * 	if the fetched value is not a valid integer.
+	 */
+	public static Color fetchColorConfig(String key) {
+		return Color.decode(fetchStringConfig("Colors", key));
 	}
 
 }
