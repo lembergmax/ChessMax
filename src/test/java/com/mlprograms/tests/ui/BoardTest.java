e11d86bbac8ad7ae5f4bfb2656776660cbf6a68f
maxle <ml.programs.d@gmail.com>
2024-12-22 12:38:21 +0100

    added few spaces

--- src/test/java/com/mlprograms/tests/ui/BoardTest.java
+++ src/test/java/com/mlprograms/tests/ui/BoardTest.java
@@ -29,6 +29,7 @@ class BoardTest {
 	@Test
 	void testInitialPieceSetup() {
 		List<Piece> pieces = board.getPieceList();
+
 		assertNotNull(pieces, "Die Liste der Figuren sollte nicht null sein.");
 		assertEquals(32, pieces.size(), "Es sollten 32 Figuren auf dem Brett sein.");
 	}
@@ -36,6 +37,7 @@ class BoardTest {
 	@Test
 	void testPieceAtPosition() {
 		Piece piece = board.getPieceAt(0, 0);
+
 		assertNotNull(piece, "Es sollte eine Figur auf der Position (0, 0) geben.");
 		assertEquals("Rook", piece.getName(), "Die Figur auf der Position (0, 0) sollte ein Turm sein.");
 	}
@@ -43,8 +45,11 @@ class BoardTest {
 	@Test
 	void testMovePiece() {
 		Piece piece = board.getPieceAt(0, 1);
+
 		assertNotNull(piece, "Es sollte eine Figur auf der Position (0, 1) geben.");
+
 		board.makeMove(new Move(board, piece, 0, 3));
+
 		assertNull(board.getPieceAt(0, 1), "Es sollte keine Figur mehr auf der Position (0, 1) geben.");
 		assertEquals(piece, board.getPieceAt(0, 3), "Die Figur sollte sich jetzt auf der Position (0, 3) befinden.");
 	}
@@ -53,6 +58,7 @@ class BoardTest {
 	void testLoadPositionFromFen() {
 		String fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 		board.loadPositionFromFen(fen);
+
 		assertEquals(32, board.getPieceList().size(), "Es sollten 32 Figuren auf dem Brett sein.");
 	}
 
@@ -60,6 +66,7 @@ class BoardTest {
 	void testMakeMove() {
 		Piece piece = board.getPieceAt(1, 1);
 		Move move = new Move(board, piece, 1, 3);
+
 		board.makeMove(move);
 		assertEquals(piece, board.getPieceAt(1, 3), "Die Figur sollte sich jetzt auf der Position (1, 3) befinden.");
 	}
@@ -68,10 +75,14 @@ class BoardTest {
 	void testCapturePiece() {
 		Piece whitePawn = new Pawn(board, 4, 6, true);
 		Piece blackPawn = new Pawn(board, 4, 4, false);
+
+		board.getPieceList().clear();
 		board.getPieceList().add(whitePawn);
 		board.getPieceList().add(blackPawn);
+
 		Move move = new Move(board, whitePawn, 4, 4);
 		board.makeMove(move);
+
 		assertNull(board.getPieceAt(4, 6), "Es sollte keine Figur mehr auf der Position (4, 6) geben.");
 		assertEquals(whitePawn, board.getPieceAt(4, 4), "Die weiße Figur sollte sich jetzt auf der Position (4, 4) befinden.");
 		assertFalse(board.getPieceList().contains(blackPawn), "Die schwarze Figur sollte entfernt worden sein.");
